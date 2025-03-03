9ec7fa8912569f9ed10e8472f51227af176eb02d
MDGeneralContracting <mdaluminum@md-contracting.com>
2025-03-03 08:06:41 -0500

    Update script.js Add autoWidth false to DataTables for alignment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // Initialize DataTables for each table with a unique ID
+    // Initialize DataTables for each table with unique ID
     $('#latest-boom-table').DataTable({
         "paging": true,
         "searching": true,
@@ -8,7 +8,8 @@ $(document).ready(function() {
         "scrollX": true,
         "responsive": true,
         "pageLength": 10,
-        "lengthMenu": [5, 10, 25, 50]
+        "lengthMenu": [5, 10, 25, 50],
+        "autoWidth": false // Prevent DataTables from setting its own width
     });
     $('#full-data-table').DataTable({
         "paging": true,
@@ -18,7 +19,8 @@ $(document).ready(function() {
         "scrollX": true,
         "responsive": true,
         "pageLength": 10,
-        "lengthMenu": [5, 10, 25, 50]
+        "lengthMenu": [5, 10, 25, 50],
+        "autoWidth": false
     });
     $('#user-summary-table').DataTable({
         "paging": true,
@@ -28,7 +30,8 @@ $(document).ready(function() {
         "scrollX": true,
         "responsive": true,
         "pageLength": 10,
-        "lengthMenu": [5, 10, 25, 50]
+        "lengthMenu": [5, 10, 25, 50],
+        "autoWidth": false
     });
     $('#builder-summary-table').DataTable({
         "paging": true,
@@ -38,7 +41,8 @@ $(document).ready(function() {
         "scrollX": true,
         "responsive": true,
         "pageLength": 10,
-        "lengthMenu": [5, 10, 25, 50]
+        "lengthMenu": [5, 10, 25, 50],
+        "autoWidth": false
     });
     console.log("M&D General Contracting tables initialized with DataTables.");
 });
