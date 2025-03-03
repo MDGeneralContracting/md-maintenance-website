94977b65aaf05b3cb93b7d185a28dae7baac0223
MDGeneralContracting <mdaluminum@md-contracting.com>
2025-03-03 08:00:48 -0500

    Update script.js fix alignment

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
-    $('.data-table').DataTable({
+    // Initialize DataTables for each table with a unique ID
+    $('#latest-boom-table').DataTable({
+        "paging": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "scrollX": true,
+        "responsive": true,
+        "pageLength": 10,
+        "lengthMenu": [5, 10, 25, 50]
+    });
+    $('#full-data-table').DataTable({
+        "paging": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "scrollX": true,
+        "responsive": true,
+        "pageLength": 10,
+        "lengthMenu": [5, 10, 25, 50]
+    });
+    $('#user-summary-table').DataTable({
+        "paging": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "scrollX": true,
+        "responsive": true,
+        "pageLength": 10,
+        "lengthMenu": [5, 10, 25, 50]
+    });
+    $('#builder-summary-table').DataTable({
         "paging": true,
         "searching": true,
         "ordering": true,
